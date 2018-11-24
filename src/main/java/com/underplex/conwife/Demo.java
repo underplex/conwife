d5c72c876edd8bdeb15d7c4301b895296076f7b9
underplex <irvinicus@yahoo.com>
2018-11-23 19:56:52 -0500

    added FileLoader

--- src/main/java/com/underplex/conwife/Demo.java
+++ src/main/java/com/underplex/conwife/Demo.java
@@ -1,23 +1,37 @@
 package com.underplex.conwife;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Demo {
 	
 	public static void main(String [] args){
-		int w = 5;
-		int h = 5;
-		String[][] array = new String[w][h];
-		for (int i = 0; i < w; i++){
-			for (int j = 0; j < h; j++){
-				if (j == 2 && i != 0 && i != 4){
-					array[i][j] = "alive";
-				} else {
-					array[i][j] = "dead";
-				}
-			}
-		}
+		
+		String fileName = "C:/Users/irvin/workspace/conwife/src/main/resources/Blinker.txt";
+		Map<String, String> statusMap = new HashMap<>();
+		int skipRows = 1;
+		
+		statusMap.put(".","dead");
+		statusMap.put("*", "alive");
+		FileLoader loader = new FileLoader(fileName, statusMap, skipRows);
+		
+//		int w = 5;
+//		int h = 5;
+//		String[][] array = new String[w][h];
+//		for (int i = 0; i < w; i++){
+//			for (int j = 0; j < h; j++){
+//				if (j == 2 && i != 0 && i != 4){
+//					array[i][j] = "alive";
+//				} else {
+//					array[i][j] = "dead";
+//				}
+//			}
+//		}
 		
 		ConwifeGame game = new ConwifeGame(new ConwayTradBlockAdvancer(),
-				w, h, array);
+				loader.getWidth(), loader.getHeight(), loader.getArray());
+		
+		game.printArray();
 		
 		int steps = 5;
 		
---
+++ src/main/java/com/underplex/conwife/FileLoader.java
@@ -0,0 +1,91 @@
+package com.underplex.conwife;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ * Utility class for loading seed files for games like Conway's Game of Life.
+ * <p>
+ * The seed values are expected to be in an array of x by y dimensions with no missing values. Missing values will be filled in by the <tt>DEFAULT_VALUE</tt> String.
+ * @author Brandon Irvine, brandon@underplex.com
+ *
+ */
+public class FileLoader {
+	
+	public static final String DEFAULT_VALUE = "undefined";
+	private final int width;
+	private final int height;
+	private final String[][] array;
+		
+	public FileLoader(String fileName, Map<String,String> statusMap, int skipRows){
+		List<String> lines = makeLinesList(fileName);
+		
+		skipRows = Math.min(skipRows, lines.size());
+		for (int i = 1; i <= skipRows; i++){
+			lines.remove(0);
+		}
+		
+		List<List<String>> temp = new ArrayList<List<String>>();
+		int greatestHeight = 0; // value of longest y coordinate yet encountered
+		
+		for (int i = 0; i < lines.size(); i++){
+			List<String> list = new ArrayList<>();
+			
+			String line = lines.get(i);
+			greatestHeight = Math.max(line.length(), greatestHeight);
+			for (int j = 0; j < line.length(); j++){
+				list.add(String.valueOf(line.charAt(j)));
+			}
+			temp.add(list);
+		}
+		
+		// now safely transfer the temp array of values to an actual array, being careful to fill in default values
+		String[][] rString = new String[lines.size()][greatestHeight];
+		for (int i = 0; i < lines.size(); i++){
+			List<String> line = temp.get(i);
+			
+			for (int j = 0; j < greatestHeight; j++){
+				String cell = null;
+				if (j < line.size()){
+					cell = statusMap.get(line.get(j));
+				}
+				// protect against null/illegal values surrounded by legal values
+				if (cell == null){
+					cell = DEFAULT_VALUE;
+				}					
+				rString[i][j] = cell;
+			}			
+		}
+		this.array = rString;
+		this.width = temp.size();
+		this.height = greatestHeight;
+	}
+		
+	private static List<String> makeLinesList(String fileName){
+	
+		List<String> lines = new ArrayList<>();
+		try (Scanner scanner = new Scanner(new File(fileName))) {
+			while (scanner.hasNextLine()){
+				lines.add(scanner.nextLine());
+			}
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+		return lines;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public String[][] getArray() {
+		return array;
+	}
+}
